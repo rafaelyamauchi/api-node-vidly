257b9c1102f2f1b0d083a2d06d6dd8cf45e01cfb
Rafael Yamauchi <rafa.yamau@hotmail.com>
2020-06-28 19:08:09 -0300

    fix the port!d

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const morgan = require('morgan');
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3900;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
